b2c23028d3c18a212d092bf226b5ab6b5771c236
Jan-Willem Buurlage <janwillembuurlage@gmail.com>
2017-04-06 10:39:40 +0200

    Update backend of GoL to use rasterific

--- examples/gameoflife/src/Main.hs
+++ examples/gameoflife/src/Main.hs
@@ -3,22 +3,19 @@ module Main where
 import Control.Monad
 import Control.Comonad
 import Diagrams.Prelude
-import Diagrams.Backend.Cairo.CmdLine
+import Diagrams.Backend.Rasterific.CmdLine
 import GameOfLife
 
-type Picture = QDiagram Cairo V2 Double Any
+type Picture = QDiagram Rasterific V2 Double Any
 
+-- Set some arbitrary initial stream
 initial = fromList [Dead, Dead, Alive, Dead, Dead, Alive]
 
+-- Construct a universe by extending these streams everywhere
 glider :: Universe Cell
 glider = Universe $ duplicate $ Tape initial Dead initial
 
-pretty :: Universe Cell -> String
-pretty u = unlines $ slice 5 5 (represent <$> u)
-  where
-    represent Dead = ' '
-    represent Alive = 'x'
-
+-- Rule and evolving
 rule :: Universe Cell -> Cell
 rule u
   | neighbours == 2 = extract u
@@ -31,13 +28,22 @@ rule u
 
 evolve u = u =>> rule
 
+-- Textual representation
+pretty :: Universe Cell -> String
+pretty u = unlines $ slice 5 5 (represent <$> u)
+  where
+    represent Dead = ' '
+    represent Alive = 'x'
+
+-- Pictorial representation
 photo :: Universe Cell -> Picture
 photo u = vcat $ hcat . map
-  (\x -> square 0.1 # fc (if x == Alive then cyan else white)) <$> slice 5 5 u
+  (\x -> square 0.1 # fc (if x == Alive then cyan else white)) <$> slice 10 10 u
 
+-- Generate frames for gif
 play :: Int -> Universe Cell -> [(Picture, Int)]
 play 0 _ = []
 play n u = (photo u, 25) : play (n - 1) (evolve u)
 
--- usage: ./gameoflife -w 500 -o anim.gif
-main = gifMain $ play 20 glider
+-- Usage: ./gameoflife -w 500 -o anim.gif
+main = mainWith $ play 20 glider
